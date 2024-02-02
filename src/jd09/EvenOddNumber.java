443ec5b346bfc55f9b079649de3e0a8112dc36fd
pendar <pendarshams@icloud.com>
2024-02-01 20:45:24 -0800

    JD09EvenOdd

---
+++ src/jd09/EvenOddNumber.java
@@ -0,0 +1,20 @@
+package jd09;
+
+public class EvenOddNumber {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7};
+        int evenCount = 0;
+        int oddCount = 0;
+
+        for (int number : array) {
+            if (number % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
+
+        System.out.println("The array has " + oddCount + " odd numbers and " + evenCount + " even numbers.");
+    }
+}
+
